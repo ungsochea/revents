f2ff057c23c150091200925e29d7304e0ca2dc49
ungsochea <sochea.ung@gmail.com>
2018-09-22 14:46:01 +0700

    End lesson 9

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
+import { reducer as FormReducer } from 'redux-form'
 import testReducer from '../../features/teatarea/testReducer'
 import eventReducer from '../../features/event/eventReducer'
 
 const rootReducer=combineReducers({
     test:testReducer,
-    events:eventReducer
+    form:FormReducer,
+    events:eventReducer,
 })
 
 export default rootReducer;
